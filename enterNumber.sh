c3fb62774f4d5382b1f2d4e2ca71646c0b14d7b0
Marzieh Rostami <51113356+MrzRst@users.noreply.github.com>
2019-12-08 17:24:09 +0330

    Create enterNumber.sh

---
+++ enterNumber.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Author Mrz.Rst
+#Date Dec-08-2019
+
+n=1
+until [ $n -eq 0 ]
+do
+        printf "Enter a number between 20 and 30:\n"
+read number
+if [ "$number" -lt 20 ]
+then
+printf "%d is too low\n again Enter:\n" "$number" >&2
+elif [ "$number" -gt 30 ]
+then
+printf "%d is too high\n again Enter:\n" "$number" >&2
+else
+printf "thank you about %d\n" "$number"
+n=0
+fi
+done
