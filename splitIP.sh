e8ad675fb25ca77861c91b540cea32784ba77d4f
Marzieh Rostami <51113356+MrzRst@users.noreply.github.com>
2019-12-08 18:07:59 +0330

    Update and rename splitIP to splitIP.sh

--- splitIP.sh
+++ splitIP.sh
@@ -2,6 +2,7 @@
 #Author Mrz.Rst
 #Date Dec-08-2019
 # script that uses parameter expansion to extract the all of number
+
 echo "Enter the IP: \n"
 read IP
 echo $IP | awk -F. '{print $1"\n"$2"\n"$3"\n"$4}'                                    
