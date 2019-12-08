fd56eba88695eb9d297638d6f7af371d3669eb4c
Marzieh Rostami <51113356+MrzRst@users.noreply.github.com>
2019-12-08 17:25:52 +0330

    Update enterNumber.sh

--- enterNumber.sh
+++ enterNumber.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #Author Mrz.Rst
 #Date Dec-08-2019
-
+#script that asks the user to enter a number between 20 and 30.
+#If the user enters an invalid number ask again. Repeat until a satisfactory number is entered. 
 n=1
 until [ $n -eq 0 ]
 do
-        printf "Enter a number between 20 and 30:\n"
+printf "Enter a number between 20 and 30:\n"
 read number
 if [ "$number" -lt 20 ]
 then
